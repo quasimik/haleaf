c920b42e9d228a571d3a1c19f8ae187b6ca84033
Nathanael See <nathanaelsee@yahoo.com>
2018-04-01 07:31:00 -0700

    Landing page, starting input form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
-import { db, auth, provider } from '../firebase';
+import React, { Component } from "react";
+import "./App.css";
+import { db, auth, provider } from "../firebase";
+import { Header, Button, Segment, Icon, Container, Grid, Menu, Input, Select, Table } from "semantic-ui-react";
 
 class App extends Component {
   
@@ -51,14 +52,77 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        {this.state.uid ?
-          <button onClick={this.logout}>Sign Out</button>
-          : <button className = "App-header" onClick={this.login}>Sign in with Google</button>
-        }
-      </div>
-    );
+
+    const age_group = [
+      {value: -1, text: "Select"},
+      {value: 1, text: "18-24"},
+      {value: 2, text: "25-29"},
+      {value: 3, text: "30-34"},
+      {value: 4, text: "35-39"},
+      {value: 5, text: "40-44"},
+      {value: 6, text: "45-49"},
+      {value: 7, text: "50-54"},
+      {value: 8, text: "55-59"},
+      {value: 9, text: "60-64"},
+      {value: 10, text: "65-69"},
+      {value: 11, text: "70-74"},
+      {value: 12, text: "75-79"},
+      {value: 13, text: ">79"},
+    ];
+
+    // Logged in
+    if (this.state.uid) {
+      return (
+        <div>
+          <Menu fixed='top' inverted>
+            <Container>
+              <Menu.Item header>HaleAF</Menu.Item>
+              <Menu.Item position="right">{this.state.user.displayName}</Menu.Item>
+              <Menu.Item onClick={this.logout}>Sign Out</Menu.Item>
+            </Container>
+          </Menu>
+          <Grid stackable centered columns={2} relaxed="very" style={{marginTop:"1em"}}>
+            <Grid.Column>
+              <Container text>
+                <Table definition>
+                  <Table.Row>
+                    <Table.Cell>
+                      Age Group: 
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Select fluid options={age_group} placeholder="Select"/>
+                    </Table.Cell>
+                  </Table.Row>
+                </Table>
+              </Container>
+            </Grid.Column>
+            <Grid.Column stretched>
+              {this.state.uid}
+            </Grid.Column>
+          </Grid>
+        </div>
+      );
+    }
+
+    // Logged out - landing page
+    else {
+      return (
+        <div className="Landing">
+          <Segment basic textAlign="center" vertical>
+            <Header as="h1" content="HaleAF"/>
+            <Header as="h2" content="Our pitch goes here???"/>
+            <Button primary size="huge" onClick={this.login} animated>
+              <Button.Content visible>
+                Get Started <Icon name="right arrow"/>
+              </Button.Content>
+              <Button.Content hidden>
+                Sign in with <Icon name="google"/>
+              </Button.Content>
+            </Button>
+          </Segment>
+        </div>
+      );
+    }
   }
 }
 
