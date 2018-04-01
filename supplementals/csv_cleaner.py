329f1e5b9c2a4ddc5bafcd2ea17d0ba150cdf68d
henrybui <henrytbui@hotmail.com>
2018-04-01 06:49:55 -0700

    Update csv_cleaner.py

--- supplementals/csv_cleaner.py
+++ supplementals/csv_cleaner.py
@@ -25,7 +25,7 @@ newcol = []
 for col in columns:
 	if col[0] == "weight_kg":
 		base = round(stat.mean( [ x for x in map(int, col[1:]) if x != -1 ] ) + 1, -1) - 40
-		categorized = [ round(( int(x) - base) / 10 , -1) for x in col[1:] ]
+		categorized = [ round(( int(x) - base) / 10) for x in col[1:] ]
 		categorized[:] = [ 1 if x < 0 else x + 2 for x in categorized ]
 		categorized[:] = [ 10 if x > 10 else x for x in categorized ]
 		categorized[:] = [ -1 if str(col[i]) == -1 else categorized[i] for i in range(1, len(data) - 1) ]
@@ -35,7 +35,7 @@ for col in columns:
 
 	elif col[0] == "height_cm":
 		base = round(stat.mean( [ x for x in map(int, col[1:]) if x != -1 ] ) + 1, -2) - 2000
-		categorized = [ round(( int(x) - base) / 500 , -2) for x in col[1:] ]
+		categorized = [ round(( int(x) - base) / 500) for x in col[1:] ]
 		categorized[:] = [ 1 if x < 0 else x + 2 for x in categorized ]
 		categorized[:] = [ 10 if x > 10 else x for x in categorized ]
 		categorized[:] = [ -1 if int(col[i]) == -1 else categorized[i] for i in range(1, len(data) - 1) ]
