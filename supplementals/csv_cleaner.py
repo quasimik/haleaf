c6053d50a019881acf614fe38a639d1eb227cad9
henrybui <henrytbui@hotmail.com>
2018-04-01 06:44:25 -0700

    Update csv_cleaner.py

--- supplementals/csv_cleaner.py
+++ supplementals/csv_cleaner.py
@@ -16,9 +16,10 @@ for rows in data:
 
 num_columns = len(columns[0])
 
-columns = [ [rows[i] for rows in columns] for i in range(0, num_columns - 1) ]
+columns = [ [rows[i] for rows in columns] for i in range(0, num_columns) ]
 
 # Categorize values 
+
 newcol = []
 for col in columns:
 	if col[0] == "weight_kg":
@@ -26,24 +27,27 @@ for col in columns:
 		categorized = [ round(( int(x) - base) / 10 , -1) for x in col[1:] ]
 		categorized[:] = [ 1 if x < 0 else x + 2 for x in categorized ]
 		categorized[:] = [ 10 if x > 10 else x for x in categorized ]
-		categorized[:] = [ -1 if str(col[i]) == -1 else categorized[i] for i in range(1, num_columns - 1) ]
-		newcol.append( [ "weight_kg" ].append(map(str, categorized)))
-			
+		categorized[:] = [ -1 if str(col[i]) == -1 else categorized[i] for i in range(1, len(data) - 1) ]
+		to_app = [ "weight_kg" ]
+		to_app.extend(categorized)
+		newcol.append( to_app )
+
 	elif col[0] == "height_cm":
 		base = round(stat.mean( [ x for x in map(int, col[1:]) if x != -1 ] ) + 1, -2) - 2000
 		categorized = [ round(( int(x) - base) / 500 , -2) for x in col[1:] ]
 		categorized[:] = [ 1 if x < 0 else x + 2 for x in categorized ]
 		categorized[:] = [ 10 if x > 10 else x for x in categorized ]
-		categorized[:] = [ -1 if int(col[i]) == -1 else categorized[i] for i in range(1, num_columns - 1) ]
-		newcol.append( [ "height_cm" ].append(map(str, categorized)))
-
+		categorized[:] = [ -1 if int(col[i]) == -1 else categorized[i] for i in range(1, len(data) - 1) ]
+		to_app = [ "height_cm" ]
+		to_app.extend( categorized )
+		newcol.append( to_app )
+		
 	else:
 		newcol.append(col)
 
 # Convert from columns to rows
-new_data = [ [cols[i] for cols in newcol] for i in range(0, len(data) - 1) ] 
-# Write categorized values to csv file
+
 f = open('categorized.csv', 'w')
-for line in newcol:
-	f.write('{}\n'.format(','.join(line)))
+for lines in newcol:
+	f.write( '{}\n'.format(','.join( map(str, lines))))
 f.close()
