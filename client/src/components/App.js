09c0a410ffead8ace647cf65f69d8268af74b504
Nathanael See <nathanaelsee@yahoo.com>
2018-04-01 03:46:31 -0700

    Firebase auth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { db, auth, provider } from '../firebase';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null,
+      uid: null,
+      ref: null,
+      data: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        const uid = user.uid;
+        const ref = db.ref(uid);
+        this.getData(ref);
+        this.setState({ user, uid, ref });
+      } 
+    });
+  }
+
+  logout = () => {
+    auth.signOut().then(() => {
+      this.setState({ user: null, uid: null, ref: null, data: null });
+    });
+  }
+
+  login = () => {
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+      const user = result.user;
+      const uid = user.uid;
+      const ref = db.ref(uid);
+      this.getData(ref);
+      this.setState({ user, uid, ref });
+    });
+  }
+
+  getData = (ref) => {
+    ref.once("value").then((snapshot) => {
+      const data = (snapshot.val() && snapshot.val().data) || {};
+      this.setState({ data });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          
-        </p>
+        {this.state.uid ?
+          <button onClick={this.logout}>Sign Out</button>
+          : <button className = "App-header" onClick={this.login}>Sign in with Google</button>
+        }
       </div>
     );
   }
